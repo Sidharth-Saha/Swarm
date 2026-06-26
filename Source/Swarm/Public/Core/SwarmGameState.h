b928c531496d9d4b237d369e56736f03212bbce8
Sidharth Saha <ssaha2000@gmail.com>
2026-06-26 11:29:38 -0700

    Create SwarmGameState.

---
+++ Source/Swarm/Public/Core/SwarmGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/Engine/Classes/GameFramework/GameStateBase.h"
+#include "SwarmGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWARM_API ASwarmGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
