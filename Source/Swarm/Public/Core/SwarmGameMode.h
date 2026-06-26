811dc32d368f2be8f917e48b9f1fbc2b2213024d
Sidharth Saha <ssaha2000@gmail.com>
2026-06-26 10:39:44 -0700

    Create SwarmGameMode.

---
+++ Source/Swarm/Public/Core/SwarmGameMode.h
@@ -0,0 +1,17 @@
+		// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwarmGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWARM_API ASwarmGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
