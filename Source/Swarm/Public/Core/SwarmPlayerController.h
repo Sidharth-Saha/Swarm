4a7cf330f445be4fd3163b71e995077b3f5ddea5
Sidharth Saha <ssaha2000@gmail.com>
2026-06-26 18:12:54 -0700

    Create SwarmPlayerController.

---
+++ Source/Swarm/Public/Core/SwarmPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+#include "SwarmPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWARM_API ASwarmPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
