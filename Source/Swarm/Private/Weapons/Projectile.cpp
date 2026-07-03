18f8500ed7a25aa28e579e58c1f033bc1bb40115
Sidharth Saha <ssaha2000@gmail.com>
2026-07-03 13:58:28 -0700

    Add mesh.

--- Source/Swarm/Private/Weapons/Projectile.cpp
+++ Source/Swarm/Private/Weapons/Projectile.cpp
@@ -3,12 +3,16 @@
 
 #include "Weapons/Projectile.h"
 
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	// Initialize the mesh
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
 }
 
 // Called when the game starts or when spawned
