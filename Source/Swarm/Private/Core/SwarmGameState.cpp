03df9fd90ff05d13cf0ff94840e4d9439f86bd82
Sidharth-Saha <ssaha2000@gmail.com>
2026-08-24 21:25:29 -0700

    Setup game states.

--- Source/Swarm/Private/Core/SwarmGameState.cpp
+++ Source/Swarm/Private/Core/SwarmGameState.cpp
@@ -1,3 +1,13 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Core/SwarmGameState.h"
+
+void ASwarmGameState::SetGameState(EGameState NewState)
+{
+	if (NewState != GameState)
+	{
+		GameState = NewState;
+		
+		OnGameStateChanged.Broadcast(NewState);
+	}
+}
--- Source/Swarm/Public/Core/SwarmGameState.h
+++ Source/Swarm/Public/Core/SwarmGameState.h
@@ -6,6 +6,16 @@
 #include "Runtime/Engine/Classes/GameFramework/GameStateBase.h"
 #include "SwarmGameState.generated.h"
 
+UENUM(BlueprintType)
+enum class EGameState : uint8 
+{
+	MainMenu,
+	Playing,
+	GameOver
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGameStateChanged, EGameState, NewState);
+
 /**
  * 
  */
@@ -13,4 +23,16 @@ UCLASS()
 class SWARM_API ASwarmGameState : public AGameStateBase
 {
 	GENERATED_BODY()
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Game State", meta = (AllowPrivateAccess = "true"))
+	EGameState GameState = EGameState::MainMenu;
+	
+public:
+	void SetGameState(EGameState NewState);
+	UFUNCTION(BlueprintCallable)
+	EGameState GetGameState() const { return GameState; }
+	
+	UPROPERTY(BlueprintAssignable, Category = "Game State")
+	FOnGameStateChanged OnGameStateChanged;
 };
