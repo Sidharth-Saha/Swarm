3256ac51c721817378361044dd3b47c4ef697030
Sidharth-Saha <ssaha2000@gmail.com>
2026-09-03 01:23:21 -0700

    Remove redundant checks.

--- Source/Swarm/Private/Weapons/Projectile.cpp
+++ Source/Swarm/Private/Weapons/Projectile.cpp
@@ -59,7 +59,7 @@ void AProjectile::Tick(float DeltaTime)
 void AProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, 
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor && OtherActor != this && OtherActor != GetInstigator())
+	if (OtherActor)
 	{
 		if (UHealthComponent* HealthComponent = OtherActor->FindComponentByClass<UHealthComponent>())
 		{
