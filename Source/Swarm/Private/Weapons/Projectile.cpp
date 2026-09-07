7b37bafa8e21007b8da7d520103c925d7b914496
Sidharth-Saha <ssaha2000@gmail.com>
2026-09-07 10:44:59 -0700

    Turn off ticking.

--- Source/Swarm/Private/Weapons/Projectile.cpp
+++ Source/Swarm/Private/Weapons/Projectile.cpp
@@ -12,7 +12,7 @@
 AProjectile::AProjectile()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	
 	// Initialize collision sphere
 	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
