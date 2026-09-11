2d75c9f65ed516fffa1738219b4a344c2583d744
Sidharth-Saha <ssaha2000@gmail.com>
2026-09-11 09:32:45 -0700

    Remove unneeded check.

--- Source/Swarm/Private/Weapons/Projectile.cpp
+++ Source/Swarm/Private/Weapons/Projectile.cpp
@@ -62,7 +62,7 @@ void AProjectile::Tick(float DeltaTime)
 void AProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, 
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor && OtherActor != this && OtherActor != GetInstigator())
+	if (OtherActor && OtherActor != this)
 	{
 		if (UHealthComponent* HealthComponent = OtherActor->FindComponentByClass<UHealthComponent>())
 		{
