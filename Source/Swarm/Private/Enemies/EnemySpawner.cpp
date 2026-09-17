4c7db15ae18bed7719272885099e770111ae03fe
Sidharth-Saha <ssaha2000@gmail.com>
2026-09-16 18:04:14 -0700

    Remove checks for now.

--- Source/Swarm/Private/Enemies/EnemySpawner.cpp
+++ Source/Swarm/Private/Enemies/EnemySpawner.cpp
@@ -19,9 +19,11 @@ void AEnemySpawner::BeginPlay()
 	Super::BeginPlay();
 	
 	ensureMsgf(GruntClass, TEXT("GruntClass not set on %s"), *GetName());
+	/*
 	ensureMsgf(RunnerClass, TEXT("RunnerClass not set on %s"), *GetName());
 	ensureMsgf(BruteClass, TEXT("BruteClass not set on %s"), *GetName());
 	ensureMsgf(ShooterClass, TEXT("ShooterClass not set on %s"), *GetName());
+	*/
 }
 
 // Called every frame
